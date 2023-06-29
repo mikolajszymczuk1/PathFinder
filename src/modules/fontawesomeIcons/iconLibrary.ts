5aae3cc9a80ad8e0e765677861cefbb575c5c2eb
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-06-29 18:36:22 +0200

    Attach Fontawesome icons to project

---
+++ src/modules/fontawesomeIcons/iconLibrary.ts
@@ -0,0 +1,23 @@
+import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
+import { faLocationPin } from "@fortawesome/free-solid-svg-icons";
+import { faFlagCheckered } from "@fortawesome/free-solid-svg-icons";
+import { faSquareXmark } from "@fortawesome/free-solid-svg-icons";
+import { faEraser } from "@fortawesome/free-solid-svg-icons";
+import { faBackwardStep } from "@fortawesome/free-solid-svg-icons";
+import { faForwardStep } from "@fortawesome/free-solid-svg-icons";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faPause } from "@fortawesome/free-solid-svg-icons";
+
+const iconsToLoad = [
+  faArrowPointer,
+  faLocationPin,
+  faFlagCheckered,
+  faSquareXmark,
+  faEraser,
+  faBackwardStep,
+  faForwardStep,
+  faPlay,
+  faPause,
+];
+
+export default iconsToLoad;
