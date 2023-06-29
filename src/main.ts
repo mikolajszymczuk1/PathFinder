e7a9babec9287b7e6d22d93661f5d65379bfff19
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-06-29 18:36:22 +0200

    Attach Fontawesome icons to project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import iconsToLoad from '@/modules/fontawesomeIcons/iconLibrary';
 import App from './App.vue';
 import './assets/css/index.css';
 
 const app = createApp(App);
 
+library.add(...iconsToLoad);
+
 app.use(createPinia());
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 
 app.mount('#app');
