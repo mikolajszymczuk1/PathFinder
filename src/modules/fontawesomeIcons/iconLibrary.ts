a07d0cdd1242f5c581f7138af4b0d8388c51acf7
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-06-30 21:12:30 +0200

    Import fix. Changed double quotes to single

--- src/modules/fontawesomeIcons/iconLibrary.ts
+++ src/modules/fontawesomeIcons/iconLibrary.ts
@@ -1,12 +1,12 @@
-import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
-import { faLocationPin } from "@fortawesome/free-solid-svg-icons";
-import { faFlagCheckered } from "@fortawesome/free-solid-svg-icons";
-import { faSquareXmark } from "@fortawesome/free-solid-svg-icons";
-import { faEraser } from "@fortawesome/free-solid-svg-icons";
-import { faBackwardStep } from "@fortawesome/free-solid-svg-icons";
-import { faForwardStep } from "@fortawesome/free-solid-svg-icons";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import { faPause } from "@fortawesome/free-solid-svg-icons";
+import { faArrowPointer } from '@fortawesome/free-solid-svg-icons';
+import { faLocationPin } from '@fortawesome/free-solid-svg-icons';
+import { faFlagCheckered } from '@fortawesome/free-solid-svg-icons';
+import { faSquareXmark } from '@fortawesome/free-solid-svg-icons';
+import { faEraser } from '@fortawesome/free-solid-svg-icons';
+import { faBackwardStep } from '@fortawesome/free-solid-svg-icons';
+import { faForwardStep } from '@fortawesome/free-solid-svg-icons';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faPause } from '@fortawesome/free-solid-svg-icons';
 
 const iconsToLoad = [
   faArrowPointer,
