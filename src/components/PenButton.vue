46829d72d40ab09d2afb64940af6efd4f3f6dfa1
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-07-15 12:23:00 +0200

    Pen Button component

---
+++ src/components/PenButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <FontAwesomeIcon
+    class=""
+    :icon="['fas', getIcon]"
+  />
+</template>
+
+<script setup lang='ts'>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { computed } from 'vue';
+
+
+
+const props = defineProps({
+  PenMode: {
+    type: String,
+    required: true,
+    validator(value: string): boolean {
+      return [].includes(value);
+    }
+  }
+});
+
+const getIcon = computed<string>(() => {
+  let icon = 'arrow-pointer';
+  switch (props.PenMode) {
+    case 'DrawStart':
+      icon = 'location-pin';
+      break;
+    case 'DrawGoal':
+      icon = 'flag-checkered';
+      break;
+    case 'DrawWall':
+      icon = 'square-xmark';
+      break;
+    case 'EraseCell':
+      icon = 'eraser';
+      break;
+  }
+  return icon;
+})
+</script>
