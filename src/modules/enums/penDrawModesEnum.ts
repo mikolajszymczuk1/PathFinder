49f850e13eae5af75e37717a2c9347210bb1df03
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-07-15 12:23:00 +0200

    Pen Button component

---
+++ src/modules/enums/penDrawModesEnum.ts
@@ -0,0 +1,9 @@
+enum PenDrawModesEnum {
+  SELECT = 'select',
+  DRAW_START = 'draw_start',
+  DRAW_GOAL = 'draw_goal',
+  DRAW_WALL = 'draw_wall',
+  ERASE_CELL = 'erase_cell',
+}
+
+export default PenDrawModesEnum;
