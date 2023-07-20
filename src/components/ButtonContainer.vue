57eb92ee881eb68aadb2d56aa9299c76521a7d57
Adrian <adrianczubaty.prywatny@gmail,com>
2023-07-20 08:35:16 +0200

    Pen select and menu component

---
+++ src/components/ButtonContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="flex w-fit px-[1rem] rounded-[0.5rem] bg-gray-500"
+    :class="flexDirection">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  flexDirection: {
+    type: String,
+    default: "flex-col",
+    required: false,
+    validator(value: string): boolean {
+      return ['flex-col', 'flex-row'].includes(value);
+    }
+  }
+});
+
+</script>
