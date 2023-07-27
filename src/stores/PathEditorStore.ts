46f7de2e708f8ed43db125cb40c927ccc3afffbc
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-07-27 21:35:47 +0200

    Added button container, pen button and path store

--- src/stores/PathEditorStore.ts
+++ src/stores/PathEditorStore.ts
@@ -1,11 +1,21 @@
 import { defineStore } from 'pinia';
+import PenDrawModesEnum from '../modules/enums/penDrawModesEnum';
 
 interface State {  // Example state interface for type definitions (change on own)
-  exampleState: string,
+  activePenMode: PenDrawModesEnum,
+
 }
 
 export const pathEditorStore = defineStore('pathEditorStore', {
   state: (): State => ({
-    exampleState: 'Hello Pinia',  // Example state (change on own)
+    activePenMode: PenDrawModesEnum.SELECT,
   }),
+  getters: {
+
+  },
+  actions: {
+    updatePen(newMode: PenDrawModesEnum): void {
+      this.activePenMode = newMode;
+    }
+  }
 });
