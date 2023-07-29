eae202eaab4b8a54bfd8567993c53128b61fd6ff
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-07-29 21:43:26 +0200

    Refactored code and added enum functions for deconstructing

--- src/components/PenButton.vue
+++ src/components/PenButton.vue
@@ -1,20 +1,20 @@
 <template>
   <FontAwesomeIcon
-    class='h-[1.75rem] py-[1.4rem] px-[.5rem] hover:text-green-200 last:border-none'
+    class='h-[1.75rem] py-[1.4rem] px-[.5rem] hover:text-green last:border-none'
     :class="isActive"
     :icon="['fas', getIcon]"
-    @click="selectDrawingTool()"
+    @click="store.selectDrawTool(penIcon)"
   />
 </template>
 
 <script setup lang='ts'>
-import PenDrawModesEnum from '@/modules/enums/penDrawModesEnum';
-import { deconstructPenDrawModes } from '@/modules/enums/penDrawModesEnum';
+import DrawModesEnum from '@/modules/enums/drawModesEnum';
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { computed } from 'vue';
 
 import { usePathEditorStore } from '@/stores/PathEditorStore';
+import { getEnumValues } from '@/modules/commonFunctions/enumHelpers';
 
 const store = usePathEditorStore();
 
@@ -23,47 +23,28 @@ const props = defineProps({
     type: String,
     required: true,
     validator(value: string): boolean {
-      return deconstructPenDrawModes().includes(value);
+      return getEnumValues(DrawModesEnum).includes(value);
     }
   }
 });
 
-const selectDrawingTool = () => {
-  let newMode = PenDrawModesEnum.SELECT;
-  switch (props.penIcon) {
-    case PenDrawModesEnum.DRAW_START:
-      newMode = PenDrawModesEnum.DRAW_START;
-      break;
-    case PenDrawModesEnum.DRAW_GOAL:
-      newMode = PenDrawModesEnum.DRAW_GOAL;
-      break;
-    case PenDrawModesEnum.DRAW_WALL:
-      newMode = PenDrawModesEnum.DRAW_WALL;
-      break;
-    case PenDrawModesEnum.ERASE_CELL:
-      newMode = PenDrawModesEnum.ERASE_CELL;
-      break;
-  }
-  store.updatePen(newMode);
-}
-
 const isActive = computed<string>(() => {
-  return props.penIcon === store.activePenMode.toString() ? "text-green-300" : 'text-gray-100';
+  return props.penIcon === store.activePenMode.toString() ? "text-lime" : 'text-white';
 });
 
 const getIcon = computed<string>(() => {
   let icon = 'arrow-pointer';
   switch (props.penIcon) {
-    case PenDrawModesEnum.DRAW_START:
+    case DrawModesEnum.DRAW_START:
       icon = 'location-pin';
       break;
-    case PenDrawModesEnum.DRAW_GOAL:
+    case DrawModesEnum.DRAW_GOAL:
       icon = 'flag-checkered';
       break;
-    case PenDrawModesEnum.DRAW_WALL:
+    case DrawModesEnum.DRAW_WALL:
       icon = 'square-xmark';
       break;
-    case PenDrawModesEnum.ERASE_CELL:
+    case DrawModesEnum.ERASE_CELL:
       icon = 'eraser';
       break;
   }
--- src/stores/PathEditorStore.ts
+++ src/stores/PathEditorStore.ts
@@ -1,6 +1,7 @@
 import type { TileCords } from '@/types/CommonTypes';
 import { defineStore } from 'pinia';
-import PenDrawModesEnum from '../modules/enums/penDrawModesEnum';
+import DrawModesEnum from '../modules/enums/drawModesEnum';
+import CellModesEnum from '@/modules/enums/cellModesEnum';
 
 interface State {
   tableData: string[][],
@@ -10,7 +11,7 @@ interface State {
 export const usePathEditorStore = defineStore('pathEditor', {
   state: (): State => ({
     tableData: [],
-    activePenMode: PenDrawModesEnum.SELECT,
+    activePenMode: DrawModesEnum.SELECT,
   }),
   actions: {
     /**
@@ -24,7 +25,7 @@ export const usePathEditorStore = defineStore('pathEditor', {
       for (let i = 0; i < height; i++) {
         const row: string[] = [];
         for (let j = 0; j < width; j++) {
-          row.push('E');
+          row.push(CellModesEnum.EMPTY);
         }
 
         this.tableData.push(row);
@@ -38,11 +39,59 @@ export const usePathEditorStore = defineStore('pathEditor', {
     */
     doOperation(cords: TileCords): void {
       const { row, col } = cords;
-      this.tableData[row][col] = 'W';
+
+      switch (this.activePenMode) {
+        case DrawModesEnum.SELECT:
+          // Do nothing
+          break;
+        case DrawModesEnum.DRAW_START:
+          this.tableData[row][col] = CellModesEnum.START;
+          break;
+
+        case DrawModesEnum.DRAW_GOAL:
+          this.tableData[row][col] = CellModesEnum.GOAL;
+          break;
+
+        case DrawModesEnum.DRAW_WALL:
+          this.tableData[row][col] = CellModesEnum.WALL;
+          break;
+
+        case DrawModesEnum.ERASE_CELL:
+        default:
+          this.tableData[row][col] = CellModesEnum.EMPTY;
+          break;
+      }
+
+      return;
     },
 
-    updatePen(newMode: string): void {
-      this.activePenMode = newMode;
-    }
+    selectDrawTool(newMode: string) {
+      if (newMode === this.activePenMode.toString()) {
+        return; // No action is needed
+      }
+
+      switch (newMode) {
+        case DrawModesEnum.SELECT:
+          this.activePenMode = DrawModesEnum.SELECT;
+          break;
+
+        case DrawModesEnum.DRAW_START:
+          this.activePenMode = DrawModesEnum.DRAW_START;
+          break;
+
+        case DrawModesEnum.DRAW_GOAL:
+          this.activePenMode = DrawModesEnum.DRAW_GOAL;
+          break;
+
+        case DrawModesEnum.DRAW_WALL:
+          this.activePenMode = DrawModesEnum.DRAW_WALL;
+          break;
+
+        case DrawModesEnum.ERASE_CELL:
+          this.activePenMode = DrawModesEnum.ERASE_CELL;
+          break;
+      }
+      return;
+    },
   },
 });
