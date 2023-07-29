3766a0747e1057ef8eff883be66865e75e392de0
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-07-29 19:20:42 +0200

    Start working on navigation control component

---
+++ src/components/EditorNavControl.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex">
+    <SingleControlButton icon-name="fa-backward-step" />
+    <div class=""></div>
+    <SingleControlButton icon-name="fa-play" />
+    <div class=""></div>
+    <SingleControlButton icon-name="fa-forward-step" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SingleControlButton from '@/components/buttons/SingleControlButton.vue';
+</script>
---
+++ src/components/buttons/SingleControlButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    class=""
+    @click="emitClick()"
+  >
+    <FontAwesomeIcon class="" :icon="['fas', iconName]" />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  iconName: {
+    type: String,
+    required: true,
+  }
+});
+
+const emit = defineEmits<{
+  /** Emit click action that can be handled */
+  (e: 'clickAction'): void
+}>();
+
+/** Emit click action that can be handled by parent component */
+const emitClick = (): void => {
+  emit('clickAction');
+}
+</script>
