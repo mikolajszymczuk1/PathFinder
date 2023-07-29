062ee51e94776692447cfd71efd880ecd82f9ef8
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-07-29 13:15:17 +0200

    Create root folder for editor and move all editor code to separate main component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
-  <div class="flex justify-center mt-[78.5px]">
-    <GridTable :table-data="store.tableData" />
-  </div>
+  <EditorMain />
 </template>
 
 <script setup lang="ts">
-import GridTable from '@/components/GridTable.vue';
-import { usePathEditorStore } from '@/stores/PathEditorStore';
-import { useWindowSize } from '@vueuse/core';
-import { getNewTilesSize } from '@/modules/commonFunctions/resizeCommon';
-
-const store = usePathEditorStore();
-const { width, height } = useWindowSize();
-const { twidth, theight } = getNewTilesSize(width, height);
-store.createTable(twidth, theight);
+import EditorMain from '@/Editor/EditorMain.vue';
 </script>
---
+++ src/Editor/EditorMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-center mt-[78.5px]">
+    <GridTable :table-data="store.tableData" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import GridTable from '@/components/GridTable.vue';
+import { usePathEditorStore } from '@/stores/PathEditorStore';
+import { useWindowSize } from '@vueuse/core';
+import { getNewTilesSize } from '@/modules/commonFunctions/resizeCommon';
+
+const store = usePathEditorStore();
+const { width, height } = useWindowSize();
+const { twidth, theight } = getNewTilesSize(width, height);
+store.createTable(twidth, theight);
+</script>
