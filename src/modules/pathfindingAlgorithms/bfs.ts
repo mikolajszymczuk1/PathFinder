da1fe9c12f90a16287e1796d97ab84c9d7903b08
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-08-12 02:05:33 +0200

    Change function for finding the shortest path for BFS and DFS to common function in separated module

--- src/modules/pathfindingAlgorithms/bfs.ts
+++ src/modules/pathfindingAlgorithms/bfs.ts
@@ -33,28 +33,3 @@ export const bfs = (grid: string[][], start: TileCords, goal: TileCords): TileCo
 
   return discoveredTiles;
 };
-
-/**
- * Function recontructs path based on discovered tiles
- * @param {string[][]} grid Area where function should search
- * @param {TileCords[]} discovered Array of discovered tiles
- * @param {TileCords} goal Cords of goal tile
- * @return {TileCords[]} Reconstructed path
- */
-export const bfsShortnesPath = (grid: string[][], discovered: TileCords[], goal: TileCords): TileCords[] => {
-  let parrent: TileCords = goal;
-  const path: TileCords[] = [];
-
-  for (let i = discovered.length - 1; i >= 0; i--) {
-    const neighbors = getNeighbors(grid, discovered[i]);
-    for (const neighbor of neighbors) {
-      if (areTilesCordsEqual(neighbor, parrent)) {
-        parrent = discovered[i];
-        path.unshift(parrent);
-        break;
-      }
-    }
-  }
-
-  return path;
-};
