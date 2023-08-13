b5c8ac1037b9b48c2901f8ff76705dd7f44fb89f
Adrian Czubaty <adriancz.prywatny@gmail.com>
2023-08-13 15:39:06 +0200

    Optimalization changes for production

--- src/modules/pathfindingAlgorithms/bfs.ts
+++ src/modules/pathfindingAlgorithms/bfs.ts
@@ -1,5 +1,5 @@
 import type { TileCords } from '@/types/CommonTypes';
-import { getNeighbours, areTilesCordsEqual } from '@/modules/commonFunctions/searchingHelpers';
+import { getNeighbors, areTilesCordsEqual, isTileCordsInArray } from '@/modules/commonFunctions/searchingHelpers';
 
 /**
  * BFS (Breadth-first search) algorithm
